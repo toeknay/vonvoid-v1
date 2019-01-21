609eedbe41cbfd9439e28fe202f88e5f64aa157a
tony <anthony@petersonrace.com>
2019-01-20 18:45:25 -0800

    base

---
+++ Gruntfile.js
@@ -0,0 +1,165 @@
+module.exports = function( grunt ) {
+
+    'use strict';
+
+    grunt.initConfig({
+
+        // Compile Sass
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'dist/css/style.css' : 'build/sass/style.scss'
+                }
+            }
+        },
+
+        // Prefix CSS
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')({ browsers: ['last 2 versions', 'Safari < 8'] }),
+                    require('postcss-flexbugs-fixes')
+                ]
+            },
+            dist: {
+                src: 'dist/css/style.css',
+                dest: 'dist/css/style.css'
+            }
+        },
+
+        // Compile HTML
+        bake: {
+            build: {
+                options: {
+                    content: "defaults.json"
+                },
+
+                files: [{
+                    expand: true,
+                    cwd: 'build/html',
+                    src: [
+                        '**.html',
+                        '**/**.html',
+                        '!modules/*.html'
+                    ],
+                    dest: 'dist/'
+                }],
+            },
+        },
+
+        // Compile JS
+        uglify: {
+            options: {
+                sourceMap: true
+            },
+            build: {
+                files: {
+                    'dist/js/main.min.js': [
+                        'build/js/jquery.min.js',
+                        'build/js/vendors/**.js',
+                        'build/js/functions.js'
+                    ],
+                }
+            }
+        },
+
+        // Compile SVGs
+        svgstore: {
+            default : {
+                files: {
+                    'build/img/svgcompile.svg': ['build/img/svgs/*.svg'],
+                },
+            },
+        },
+
+        // Sync Images & Fonts
+        sync: {
+            main: {
+                files: [
+                    {
+                        cwd: 'build/img',
+                        src: ['**'],
+                        dest: 'dist/img'
+                    },
+                    {
+                        cwd: 'build/fonts',
+                        src: ['**'],
+                        dest: 'dist/fonts'
+                    }
+                ],
+                updateAndDelete: true
+            }
+        },
+
+        // Watch for file changes
+        watch: {
+            compass: {
+                files: [
+                    'build/sass/*.{scss,sass}',
+                    'build/sass/**/**.{scss,sass}'
+                ],
+                tasks:['sass', 'postcss:dist']
+            },
+            svgstore: {
+                files: [
+                    'build/img/svgs/*.svg'
+                ],
+                tasks:['svgstore']
+            },
+            sync: {
+                files: [
+                    'build/img/**',
+                    'build/fonts/**'
+                ],
+                tasks:['sync']
+            },
+            bake: {
+                files: [
+                    'build/html/**',
+                    'defaults.json'
+                ],
+                tasks: ['bake:build']
+            },
+            uglify: {
+                files: [
+                    'build/js/**',
+                    '!build/js/main.min.js',
+                    '!build/js/main.min.js.map'
+                ],
+                tasks: ['uglify']
+            }
+        },
+
+        // Reload browser
+        browserSync: {
+            files: [ 'dist/**'],
+            options: {
+                server: {
+                    baseDir: 'dist',
+                    serveStaticOptions: {
+                        extensions: ['html']
+                    }
+                },
+                open: true,
+                watchTask: true,
+                injectChanges: false
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-bake');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-svgstore');
+    grunt.loadNpmTasks('grunt-sync');
+    grunt.loadNpmTasks('grunt-browser-sync');
+
+    grunt.registerTask('default', ['sync','sass','postcss:dist','svgstore','bake:build','uglify','browserSync','watch']);
+};
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# vonvoid-v1
+# Static Site Builder with Grunt
+
+This is everything you need to build a static website, using Grunt to compile your HTML, CSS & JS with livereload.
+
+#### Setup
+
+- Open terminal, `cd` to where you keep your sites.
+- Clone this repo.
+- In terminal run `npm install` to install the projects dependancies.
+- Once complete, in terminal run `grunt` to get started.
+
+#### Build & Dist
+- The `build` folder is where you will do all of your work.
+- The `dist` folder is where everything in the `build` with compile to.
+
+#### Adding HTML Pages
+- Add your new page in `build/html`.
+- Use `build/html/modules` for components that a repeated across your site such as a header.
+- Anything that is placed outside `build/html/modules` will be compiled into `dist`
+
+#### Adding JS
+- Add a new JS file to `build/js/vendors` and it will auto compile.
+- Any JS that you want to do can be done in `build/js/functions.js`
+
+#### Adding SVGs
+- Add your SVG to `build/img/svgs` with a desired file name.
+- Now add the SVG to your HTML by referencing to it `<svg><use xlink:href="#svg-file-name"/></svg>`.
+- To ensure that your SVG scales correctly you'll need to add `viewBox` to `<svg>`.
+  - To find the `viewBox` first go into your SVG file `build/img/svgs` and you'll see something like this `viewBox="0 0 295.2 277.2"` copy it.
+  - Paste this into the `<svg>`, it should look something like this `<svg viewBox="0 0 295.2 277.2"><use xlink:href="#svg-file-name"/></svg>`
+
+#### Useful Links
+- Sass Architecture Structure | http://git.io/6SmiOw
+- CSS Declaration order | http://git.io/vJ7cU
+- CSS Guidelines | http://cssguidelin.es/
+- Create Favicons | http://realfavicongenerator.net/
